2f706f9a3d9c20e25b44ec29516dc0967c203b42
sukhv <sukhvindersingh2513@gmail.com>
2019-02-08 01:08:53 +0530

    getting api with id

--- server/server.js
+++ server/server.js
@@ -1,34 +1,55 @@
-var express=require('express');
-var bodyparser=require('body-parser');
+var express = require('express');
+var bodyparser = require('body-parser');
 
-var {mongoose}=require('./db/mongoose');
-var {Todo}=require('./models/todo');
-var {User}=require('./models/User');
-
-var app=express();
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/User');
+const { ObjectID } = require('mongodb');
+var app = express();
 
 app.use(bodyparser.json());
 
-app.post('/todos',(req,res)=>{
+app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text: req.body.text 
+        text: req.body.text
     });
-    todo.save().then((docs)=>{
+    todo.save().then((docs) => {
         res.send(docs);
-    },(err)=>{
+    }, (err) => {
         res.status(400).send(err);
     })
 });
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
         res.send(todos);
-    },(err)=>{
+    }, (err) => {
         res.status(400).send(err);
     })
 });
 
-app.listen(3000,()=>{
-console.log('listening to port no 3000');
+app.get('/todos/:id', (req, res) => {
+    // res.send(req.params);
+    var id = req.params.id;
+    // console.log(id);
+    if (!ObjectID.isValid(id)) {
+        // console.log('Id is not valid');
+        return res.status(404).send();
+    } 
+        Todo.findById(id).then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+            res.send({todo });
+        }).catch((e) => {
+            res.status(400).send();
+        })
+    
+
+
+})
+
+app.listen(3000, () => {
+    console.log('listening to port no 3000');
 });
 
-module.exports={app};
+module.exports = { app };
